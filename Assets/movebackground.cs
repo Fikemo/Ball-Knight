c7c016c09d33c1c85814daa5c85e585e9411bf9c
Bryson-Frank <bmfrank@ucsc.edu>
2021-11-29 19:34:48 -0800

    background and script

---
+++ Assets/movebackground.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movebackground : MonoBehaviour
+{
+    public CameraScroll cameraScroll;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
