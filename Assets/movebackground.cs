6dae8ab4118f1f2224321ae650c84d0b4e12b6b3
Edywang <edwinwang00@gmail.com>
2021-11-29 20:32:48 -0800

    Update movebackground.cs

--- Assets/movebackground.cs
+++ Assets/movebackground.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class movebackground : MonoBehaviour
 {
-    public CameraScroll cameraScroll;
+    private CameraScroll cameraScroll;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Grab CameraScroll from the MainCamera
+        cameraScroll = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraScroll>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Move the background to the left by half of the camera's speed
+        transform.position.x -= cameraScroll.scrollSpeed / 2.0 * Time.deltaTime;
     }
 }
